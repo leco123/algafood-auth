594df4cfc7747183eca0f95b0bfb5a5841c980c2
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-12-20 14:44:30 -0300

    22.9. Configurando o fluxo Authorization Server com Password Credentials e Opaque Tokens

--- src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -1,11 +1,52 @@
 package com.algaworks.algafood.auth;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    /**
+     * Define que determinado usuário usando password *** pode usar o fluxo: password
+     */
+    @Override
+    public void configure (ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                    // identificação do cliente
+                    .withClient("algafood-web")
+                    .secret(passwordEncoder.encode("web123"))
+                    // São fluxos
+                    .authorizedGrantTypes("password")
+                    // Tipos de escopo
+                    .scopes("write","read")
+                    .accessTokenValiditySeconds(60 * 60 * 6)
+                .and()
+                    .withClient("algafood-mobile")
+                    .secret(passwordEncoder.encode("abc555"))
+                    .authorizedGrantTypes("password")
+                    .scopes("write","read")
+                    .accessTokenValiditySeconds(60 * 60 * 6);
+    }
+
+    /**
+     * Esse método é usado apenas para o Fluxo password
+     */
+    @Override
+    public void configure (AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
 }
